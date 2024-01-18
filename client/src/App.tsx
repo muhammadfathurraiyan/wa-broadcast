3a336f9850b9228486fdc1759306bc0285f76d6a
muhammadfathurraiyan <raiyanfathur@gmail.com>
2024-01-19 00:33:48 +0700

    update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,41 +4,66 @@ import { io } from "socket.io-client";
 
 function App() {
   const [result, setResult] = useState("");
+  const [status, setStatus] = useState("");
   const [id, setId] = useState("");
+  const socket = io("http://localhost:3000");
 
   const apiCall = async () => {
-    const socket = io("http://localhost:3000");
-    socket.emit(id);
+    socket.emit("id", id);
   };
 
-  return (
-    <main className="max-w-2xl mx-auto gap-4 flex flex-col mt-16">
-      <h1 className="text-center text-neutral-900 uppercase font-bold text-3xl">
-        Whatsapp Broadcast
-      </h1>
-      <div className="flex flex-col items-center justify-center">
-        <div className="flex w-[400px] gap-4">
-          <input
-            type="text"
-            placeholder="masukan id"
-            name="id"
-            value={id}
-            onChange={(e) => setId(e.target.value)}
-            className="p-2 w-full border border-neutral-700 outline-none"
-          />
-          <button
-            onClick={apiCall}
-            className="bg-neutral-900 py-2 px-4 w-fit text-neutral-200"
-          >
-            Submit
-          </button>
+  const sendMessage = async () => {
+    socket.emit("chat", "hello");
+  };
+
+  socket.on("qr", (qr) => {
+    setResult(qr);
+  });
+
+  socket.on("status", (ready) => {
+    setStatus(ready);
+  });
+
+  if (status === "ready") {
+    return (
+      <main className="max-w-2xl mx-auto gap-4 flex flex-col mt-16">
+        <h1 className="text-center text-neutral-900 uppercase font-bold text-3xl">
+          Whatsapp Broadcast
+        </h1>
+        <p>Ready</p>
+        <button onClick={sendMessage}>kirim</button>
+      </main>
+    );
+  } else {
+    return (
+      <main className="max-w-2xl mx-auto gap-4 flex flex-col mt-16">
+        <h1 className="text-center text-neutral-900 uppercase font-bold text-3xl">
+          Whatsapp Broadcast
+        </h1>
+        <div className="flex flex-col items-center justify-center">
+          <div className="flex w-[400px] gap-4">
+            <input
+              type="text"
+              placeholder="masukan id"
+              name="id"
+              value={id}
+              onChange={(e) => setId(e.target.value)}
+              className="p-2 w-full border border-neutral-700 outline-none"
+            />
+            <button
+              onClick={apiCall}
+              className="bg-neutral-900 py-2 px-4 w-fit text-neutral-200"
+            >
+              Submit
+            </button>
+          </div>
         </div>
-      </div>
-      <div className="flex justify-center">
-        {result !== "" && <QRCode fgColor="rgb(23 23 23)" value={result} />}
-      </div>
-    </main>
-  );
+        <div className="flex justify-center">
+          {result !== "" && <QRCode fgColor="rgb(23 23 23)" value={result} />}
+        </div>
+      </main>
+    );
+  }
 }
 
 export default App;
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
+import { Client, Contact, LocalAuth } from "whatsapp-web.js";
 
 const app = express();
 const httpServer = createServer(app);
@@ -9,7 +10,33 @@ const io = new Server(httpServer, {
 });
 
 io.on("connection", (socket) => {
-  console.log("conected from:", socket.id);
+  let client: Client;
+  socket.on("id", (id) => {
+    client = new Client({
+      authStrategy: new LocalAuth({ clientId: id }),
+    });
+
+    client.on("qr", (qr) => {
+      console.log("QR RECEIVED", qr);
+      socket.emit("qr", qr);
+    });
+
+    client.on("ready", () => {
+      console.log("Client is ready!");
+      socket.emit("status", "ready");
+    });
+    client.initialize();
+  });
+
+  socket.on("chat", (chat) => {
+    client.on("ready", () => {
+      const number = "+6282361564525";
+      const text = chat;
+      const chatId = number.substring(1) + "@c.us";
+      client.sendMessage(chatId, text);
+    });
+    client.initialize();
+  });
 });
 
 httpServer.listen(3000);
